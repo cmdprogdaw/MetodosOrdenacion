d2186687129042a2dcf5cb51dd10c199ea8a8942
Cristina <cristinamegi.cmd@gmail.com>
2018-12-19 14:17:17 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Main extends JFrame {
 	
-	public Main() { //consructor de la clase
-		setTitle("Metodos de Ordenación"); //titulo de la ventana
+	public Main() { //constructor de la clase
+		setTitle("Metodos de Ordenaciï¿½n"); //titulo de la ventana
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //que cuando se cierre la ventana JFrame se finalice el programa
 		getContentPane().add(new Lienzo());
 		pack();
 	}
 	
 	public static void main(String[] args) {
-		new Main().setVisible(true); //crea un objeto de la clase main, aqunque no se asigne a nadie no queda desreferenciado (y te lo crees y punto porque lo dice Julio)
+		SwingUtilities.invokeLater(
+				new Runnable() {
+					@Override
+					public void run() {
+						new Main().setVisible(true); //crea un objeto de la clase main, aunque no se asigne a nadie no queda desreferenciado (y te lo crees y punto porque lo dice Julio)
+					}
+				}			
+		);	
+		
 	}
 
 }
